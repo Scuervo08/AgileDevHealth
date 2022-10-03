d8761804d0f646f4148565b0fd64791d600efa73
Alejitap <possealeja@gmail.com>
2022-10-03 14:54:48 -0500

    Serializadores y vistas MOdelo medicoIII

--- agildevhealtproject/urls.py
+++ agildevhealtproject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-from AgileDevHealthApp import views
+from agildevhealtapp import views
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
